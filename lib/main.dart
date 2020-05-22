61e663b7e72d989692af9da91ded0f85efb1cda3
yuliefas <yuliefas@gmail.com>
2020-05-22 21:06:11 +0700

    add FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,12 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: FlatButton(
+        onPressed: () {
+          print('I got clicked');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
