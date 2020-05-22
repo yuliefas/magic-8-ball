5eda3142a888db157fb40a2b64b0542ca1ed131f
yuliefas <yuliefas@gmail.com>
2020-05-22 21:40:11 +0700

    add Random math

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -30,14 +31,22 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void setupBallNumber() {
+    ballNumber = Random().nextInt(5) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
         onPressed: () {
-          print('I got clicked');
+          setState(() {
+            setupBallNumber();
+          });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
