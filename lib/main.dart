92aebfc7afde51734493bb1705008ea0df4da08c
yuliefas <yuliefas@gmail.com>
2020-05-22 20:59:29 +0700

    Boss Level challenge 1, using stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          'Ask Me anything',
+        ),
       ),
+      body: Container(),
     );
+  }
+}
